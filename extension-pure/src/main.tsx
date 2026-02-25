137e649e5cc4627e731d45b53f0b145db8074222
marctang <marctang@tencent.com>
2026-02-25 16:53:18 +0800

    feat: 修改参数

--- extension-pure/src/main.tsx
+++ extension-pure/src/main.tsx
@@ -3,8 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import '@/styles/mobile.css'
 import App from './App'
-import '@/i18n'
+import i18n from '@/i18n'
 import '@/i18n/types'
+
+if (!localStorage.getItem('i18nextLng')) {
+  i18n.changeLanguage('zh-CN');
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
